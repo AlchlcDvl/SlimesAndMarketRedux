055980c7613faa54f0ef9ddd835674172439854c
AlchlcDvl <56699741+AlchlcDvl@users.noreply.github.com>
2025-07-04 14:38:02 -0400

    Clean up codebase + fix unsellable slimes bug + improve compatibility

--- SlimesAndMarket/ExtraSlimes.cs
+++ SlimesAndMarket/ExtraSlimes.cs
@@ -1,119 +1,53 @@
-﻿using SRML.SR;
-using SRML.Console;
-using System.Collections.Generic;
+﻿using SRML;
+using SRML.SR;
 
-namespace SlimesAndMarket
+namespace SlimesAndMarket;
+
+public static class ExtraSlimes
 {
-    class ExtraSlimes
+    public static readonly List<(Identifiable.Id, Identifiable.Id)> VANILLA_SLIMES =
+    [
+        (Identifiable.Id.PINK_SLIME, Identifiable.Id.PINK_PLORT),
+        (Identifiable.Id.TABBY_SLIME, Identifiable.Id.TABBY_PLORT),
+        (Identifiable.Id.ROCK_SLIME, Identifiable.Id.ROCK_PLORT),
+        (Identifiable.Id.PHOSPHOR_SLIME, Identifiable.Id.PHOSPHOR_PLORT),
+        (Identifiable.Id.RAD_SLIME, Identifiable.Id.RAD_PLORT),
+        (Identifiable.Id.BOOM_SLIME, Identifiable.Id.BOOM_PLORT),
+        (Identifiable.Id.CRYSTAL_SLIME, Identifiable.Id.CRYSTAL_PLORT),
+        (Identifiable.Id.HUNTER_SLIME, Identifiable.Id.HUNTER_PLORT),
+        (Identifiable.Id.HONEY_SLIME, Identifiable.Id.HONEY_PLORT),
+        (Identifiable.Id.QUANTUM_SLIME, Identifiable.Id.QUANTUM_PLORT),
+        (Identifiable.Id.DERVISH_SLIME, Identifiable.Id.DERVISH_PLORT),
+        (Identifiable.Id.TANGLE_SLIME, Identifiable.Id.TANGLE_PLORT),
+        (Identifiable.Id.MOSAIC_SLIME, Identifiable.Id.MOSAIC_PLORT),
+        (Identifiable.Id.PUDDLE_SLIME, Identifiable.Id.PUDDLE_PLORT),
+        (Identifiable.Id.FIRE_SLIME, Identifiable.Id.FIRE_PLORT),
+    ];
+
+    public static EconomyDirector.ValueMap GetValueMap(Identifiable.Id id)
     {
-        public static List<Identifiable.Id> VANILLA_SLIMES = new List<Identifiable.Id>()
+        //Everything that can be sold
+        foreach (var valueMap in SRSingleton<SceneContext>.Instance.EconomyDirector.baseValueMap)
         {
-            Identifiable.Id.PINK_SLIME,
-            Identifiable.Id.TABBY_SLIME,
-            Identifiable.Id.ROCK_SLIME,
-            Identifiable.Id.PHOSPHOR_SLIME,
-            Identifiable.Id.RAD_SLIME,
-            Identifiable.Id.BOOM_SLIME,
-            Identifiable.Id.CRYSTAL_SLIME,
-            Identifiable.Id.HUNTER_SLIME,
-            Identifiable.Id.HONEY_SLIME,
-            Identifiable.Id.QUANTUM_SLIME,
-            Identifiable.Id.DERVISH_SLIME,
-            Identifiable.Id.TANGLE_SLIME,
-            Identifiable.Id.MOSAIC_SLIME,
+            // Main.Instance.ConsoleInstance.Log("GetValueMap: " + valueMap.accept.id);
+
+            if (id == valueMap.accept.id)
+                return valueMap;
+        }
+
+        return null;
+    }
+
+    public static void RegisterSlime(Identifiable.Id slimeId, Identifiable.Id plortId, float multiplier = 4f)
+    {
+        var valueMap = GetValueMap(plortId);
+        var (value, saturation) = slimeId switch
+        {
+            Identifiable.Id.QUICKSILVER_SLIME when plortId == 0 => (200f, 100f),
+            Identifiable.Id.LUCKY_SLIME when plortId == 0 => (250f, 125f),
+            _ => (valueMap.value, valueMap.fullSaturation)
         };
-
-        public static EconomyDirector.ValueMap GetValueMap(Identifiable.Id id)
-        {
-            EconomyDirector.ValueMap[] valueMaps = SRSingleton<SceneContext>.Instance.EconomyDirector.baseValueMap; //Everything that can be sold
-
-            foreach (EconomyDirector.ValueMap valueMap in valueMaps)
-            {
-                Console.Log("GetValueMap: " + valueMap.accept.id);
-                if (id == valueMap.accept.id)
-                {
-                    return valueMap;
-                }
-            }
-
-            return null;
-        }
-        public static void RegisterSaberSlimes()
-        {
-            Identifiable.Id id = Identifiable.Id.SABER_PLORT;
-            Identifiable.Id slimeId = Identifiable.Id.SABER_SLIME;
-
-            EconomyDirector.ValueMap valueMap = GetValueMap(id);
-
-            PlortRegistry.RegisterPlort(slimeId, valueMap.value * 50, valueMap.fullSaturation);
-
-            DroneRegistry.RegisterBasicTarget(slimeId); //And make so that the drones can grab slimes
-
-            Console.Log("Slime:" + slimeId + " registred perfectly");
-        }
-
-        public static void RegisterGoldSlimes()
-        {
-            Identifiable.Id id = Identifiable.Id.GOLD_PLORT;
-            Identifiable.Id slimeId = Identifiable.Id.GOLD_SLIME;
-
-            EconomyDirector.ValueMap valueMap = GetValueMap(id);
-
-            PlortRegistry.RegisterPlort(slimeId, valueMap.value * 8, valueMap.fullSaturation);
-
-            DroneRegistry.RegisterBasicTarget(slimeId); //And make so that the drones can grab slimes
-
-            Console.Log("Slime:" + slimeId + " registred perfectly");
-        }
-
-        public static void RegisterPuddleSlimes()
-        {
-            Identifiable.Id id = Identifiable.Id.PUDDLE_PLORT;
-            Identifiable.Id slimeId = Identifiable.Id.PUDDLE_SLIME;
-
-            EconomyDirector.ValueMap valueMap = GetValueMap(id);
-
-            PlortRegistry.RegisterPlort(slimeId, valueMap.value * 4, valueMap.fullSaturation);
-
-            DroneRegistry.RegisterBasicTarget(slimeId); //And make so that the drones can grab slimes
-
-            Console.Log("Slime:" + slimeId + " registred perfectly");
-        }
-
-        public static void RegisterFireSlimes()
-        {
-            Identifiable.Id id = Identifiable.Id.FIRE_PLORT;
-            Identifiable.Id slimeId = Identifiable.Id.FIRE_SLIME;
-
-            EconomyDirector.ValueMap valueMap = GetValueMap(id);
-
-            PlortRegistry.RegisterPlort(slimeId, valueMap.value * 4, valueMap.fullSaturation);
-
-            DroneRegistry.RegisterBasicTarget(slimeId); //And make so that the drones can grab slimes
-
-            Console.Log("Slime:" + slimeId + " registred perfectly");
-        }
-
-        public static void RegisterQuicksilverSlimes()
-        {
-            Identifiable.Id slimeId = Identifiable.Id.QUICKSILVER_SLIME;
-
-            PlortRegistry.RegisterPlort(slimeId, 200f*5, 100f);
-
-            DroneRegistry.RegisterBasicTarget(slimeId); //And make so that the drones can grab slimes
-
-            Console.Log("Slime:" + slimeId + " registred perfectly");
-        }
-
-        public static void RegisterLuckySlimes()
-        {
-            Identifiable.Id slimeId = Identifiable.Id.LUCKY_SLIME;
-
-            PlortRegistry.RegisterPlort(slimeId, 250f*20, 125f);
-
-            DroneRegistry.RegisterBasicTarget(slimeId); //And make so that the drones can grab slimes
-
-            Console.Log("Slime:" + slimeId + " registred perfectly");
-        }
+        PlortRegistry.RegisterPlort(slimeId, value * multiplier, saturation);
+        DroneRegistry.RegisterBasicTarget(slimeId); // And make so that the drones can grab slimes
     }
 }
--- SlimesAndMarket/Main.cs
+++ SlimesAndMarket/Main.cs
@@ -1,173 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using SRML;
 
-using SRML;
-using SRML.SR;
+namespace SlimesAndMarket;
 
-using Console = SRML.Console.Console;
-
-namespace SlimesAndMarket
+public class Main : ModEntryPoint
 {
-    public class Main : ModEntryPoint
+    public static Main Instance { get; private set; }
+
+    public override void PreLoad()
     {
-        public override void PreLoad()
-        {
-            HarmonyInstance.PatchAll();
-        }
-
-        public override void Load()
-        {
-            Console.Log("Loading SlimesAndMarket");
-
-            try //Try the code there, if some error arises then it calls the "catch" function 
-            {
-                foreach (Identifiable.Id slime in ExtraSlimes.VANILLA_SLIMES) //For each slime int he slime class
-                {
-                    Console.Log("Printing Slime: " + slime.ToString());
-
-                    EconomyDirector.ValueMap valueMap = Extension.GetValueMap(slime);
-                    
-                    //If the slime is a puddle/fire/quicksilver slime then add the plort id, else just add the id of the slime (true/false argumeter tells the programm if it's a puddle/fire/quicksilver slime or regular slimes, except tarrs and lucky slimes)
-                    //We are getting the informations of the plort of the correct slime (to make normal prices)
-                    //But if we find nothing then return null
-                    Console.Log("   valueMap not found...");
-                    if (valueMap == null)
-                        continue;
-                    Console.Log("   ...or maybe yes? Now registering slimes");
-
-                    PlortRegistry.RegisterPlort(slime, valueMap.value * 4, valueMap.fullSaturation); //Else, if the slime is a regular slime, then register the slime (make so that you can sell it) for 4 times the original price more.
-
-                    DroneRegistry.RegisterBasicTarget(slime); //And make so that the drones can grab slimes
-
-                    Console.Log("Slime: " + slime + " registred perfectly");
-                }
-            }
-            catch (Exception e) //But if an error was found
-            {
-                Console.Log("Slimes and Market mod error (Vanilla): " + e.Message); //Then log the error into the SRML.log file
-            }
-
-            ExtraSlimes.RegisterSaberSlimes();
-            ExtraSlimes.RegisterGoldSlimes();
-            ExtraSlimes.RegisterFireSlimes();
-            ExtraSlimes.RegisterPuddleSlimes();
-            ExtraSlimes.RegisterQuicksilverSlimes();
-            ExtraSlimes.RegisterLuckySlimes();
-        }
-
+        Instance = this;
+        HarmonyInstance.PatchAll();
     }
 
-    public static class Extension
+    public override void Load()
     {
-        static List<Identifiable.Id> registredSlimes = new List<Identifiable.Id>(); //A List used to mark what slimes already got registred and what not
-        static List<Identifiable.Id> largoSlimes = LargoLibrary.LargoGenerator.generatedIds;
+        ConsoleInstance.Log("Loading SlimesAndMarket");
 
-        public static bool FindContent(this Array obj, Identifiable.Id comparator, string print = "") //This is an extension (it's called so in C#) for arrays that allows me to search if something is in that array or not (without erroring).
+        try // Try the code there, if some error arises then it calls the "catch" function
         {
-            if (print != "")
-                //Console.Log("Printing Array: " + print);
-                foreach (Identifiable.Id content in obj) //For each element in our array
-                {
-                    if (print != "")
-                        //Console.Log("Printing Id: " + content);
-                        if (content == comparator) //If the ID is there
-                        {
-                            //Console.Log("   Something got found in the comparator: " + content);
-                            return true; //Then return true
-                        }
-                }
-            //Console.Log("   Nothing got found in the comparator");
-            return false; //Else return false
-        }
+            ExtraSlimes.VANILLA_SLIMES.ForEach(x => ExtraSlimes.RegisterSlime(x.Item1, x.Item2));
 
-        public static bool IsNullOrEmpty<T>(this T[] array) //To see if an array exists and/or has 
-        {
-            if (array == null || array.Length == 0)
-                return true;
-            else
-                return array.All(item => item == null);
-        }
+            // Only loading the special slime sales if relevant mods are enabled, because there would be no other way the player would be able to sell them otherwise
 
-        public static EconomyDirector.ValueMap FindById(this Array obj, string puddleSlime)
-        {
-            Identifiable.Id acceptId = Identifiable.Id.NONE;
-            Identifiable.Id acceptIdPlort = Identifiable.Id.NONE;
+            if (SRModLoader.IsModPresent("komikspl_quicksilver_rancher"))
+                ExtraSlimes.RegisterSlime(Identifiable.Id.QUICKSILVER_SLIME, Identifiable.Id.QUICKSILVER_PLORT, 5f);
 
-            foreach (EconomyDirector.ValueMap valueMap in obj)
+            if (SRModLoader.IsModPresent("puresaberslime"))
+                ExtraSlimes.RegisterSlime(Identifiable.Id.SABER_SLIME, Identifiable.Id.SABER_PLORT, 50f);
+
+            if (SRModLoader.IsModPresent("more_vaccing"))
             {
-                if (puddleSlime == "PUDDLE_SLIME")
-                {
-                    acceptId = Identifiable.Id.PUDDLE_SLIME;
-                    acceptIdPlort = Identifiable.Id.PUDDLE_PLORT;
-                }
-                else
-                {
-                    acceptId = Identifiable.Id.FIRE_SLIME;
-                    acceptIdPlort = Identifiable.Id.FIRE_PLORT;
-                }
-
-                if (!registredSlimes.ToArray().FindContent(acceptId))
-                { //If the slime wasn't registred before, then continue, else dont continue
-
-                    if (valueMap.accept.id == acceptIdPlort) //If the Identifiable.Id matches with the plort already registred
-                    {
-                        registredSlimes.Add(acceptId); //Add this slime to the eatmap: now it's registred, and it cannot be registred again
-                        return valueMap; //Then we found a matching slime, so return the correct valueMap (informations about the plort that you normally sell)
-                    }
-                }
+                ExtraSlimes.RegisterSlime(Identifiable.Id.GOLD_SLIME, Identifiable.Id.GOLD_PLORT, 10f);
+                ExtraSlimes.RegisterSlime(Identifiable.Id.LUCKY_SLIME, SRModLoader.IsModPresent("dogeiscutLuckyPlorts") && Enum.TryParse<Identifiable.Id>("LUCKY_PLORT", out var plort) ? plort : 0,
+                    50f);
             }
-            return null;
         }
-
-        public static EconomyDirector.ValueMap FindById(this Array obj, Identifiable.Id id)
+        catch (Exception e) // But if an error was found
         {
-            foreach (EconomyDirector.ValueMap valueMap in obj) //For each vanilla sellable plorts
-            {
-                bool check = true;
-                Identifiable.Id acceptId = Identifiable.Id.NONE;
-
-                if (check)
-                {
-                    if (SRSingleton<GameContext>.Instance.SlimeDefinitions.GetSlimeByIdentifiableId(id).Diet.Produces.IsNullOrEmpty()) //If the "plort" parameter is not a slime and the slime produces no plorts, avoid the error and just pass on the next slime
-                        break;
-                }
-
-                if (check)
-                    acceptId = SRSingleton<GameContext>.Instance.SlimeDefinitions.GetSlimeByIdentifiableId(id).Diet.Produces[0];
-
-                //Identifiable.Id acceptId = (plort ? id : SRSingleton<GameContext>.Instance.SlimeDefinitions.GetSlimeByIdentifiableId(id).Diet.Produces[0]); //Ternary Operator: If Parameter 3, "boolean plort", [Continue below]
-                //is true, then just make the control with Parameter 2 (it is a plort), else get the plort of the slime (use Parameter 2 as the id of the current slime)
-
-                if (!registredSlimes.ToArray().FindContent(acceptId))
-                { //If the slime wasn't registred before, then continue, else dont continue
-
-                    if (valueMap.accept.id == acceptId) //If the Identifiable.Id matches with the plort already registred
-                    {
-                        registredSlimes.Add(acceptId); //Add this slime to the eatmap: now it's registred, and it cannot be registred again
-                        return valueMap; //Then we found a matching slime, so return the correct valueMap (informations about the plort that you normally sell)
-                    }
-                }
-            }
-
-            return null; //If nothing was found then return null
+            ConsoleInstance.LogError("Slimes and Market mod error: " + e); // Then log the error into the SRML.log file
         }
-        
-        public static EconomyDirector.ValueMap GetValueMap(Identifiable.Id id)
-        {
-            EconomyDirector.ValueMap[] valueMaps = SRSingleton<SceneContext>.Instance.EconomyDirector.baseValueMap; //Everything that can be sold
-
-            foreach (EconomyDirector.ValueMap valueMap in valueMaps)
-            {
-                Console.Log("GetValueMap: " + valueMap.accept.id);
-                if (SRSingleton<GameContext>.Instance.SlimeDefinitions.GetSlimeByIdentifiableId(id).Diet.Produces[0] == valueMap.accept.id)
-                {
-                    return valueMap;
-                }
-            }
-
-            return null;
-        }
-
     }
 }
