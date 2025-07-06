2b219d29459565101b09c007aaf9808b978ecf2e
AlchlcDvl <56699741+AlchlcDvl@users.noreply.github.com>
2025-07-06 14:40:56 -0400

    1.4.1
    - Allow game progress based unlocks, same as the base game plorts
    - Minor optimisations to value handling

--- SlimesAndMarket/ExtraSlimes.cs
+++ SlimesAndMarket/ExtraSlimes.cs
@@ -4,26 +4,7 @@ namespace SlimesAndMarket;
 
 public static class ExtraSlimes
 {
-    public static readonly List<(Identifiable.Id, Identifiable.Id)> VANILLA_SLIMES =
-    [
-        (Identifiable.Id.PINK_SLIME, Identifiable.Id.PINK_PLORT),
-        (Identifiable.Id.TABBY_SLIME, Identifiable.Id.TABBY_PLORT),
-        (Identifiable.Id.ROCK_SLIME, Identifiable.Id.ROCK_PLORT),
-        (Identifiable.Id.PHOSPHOR_SLIME, Identifiable.Id.PHOSPHOR_PLORT),
-        (Identifiable.Id.RAD_SLIME, Identifiable.Id.RAD_PLORT),
-        (Identifiable.Id.BOOM_SLIME, Identifiable.Id.BOOM_PLORT),
-        (Identifiable.Id.CRYSTAL_SLIME, Identifiable.Id.CRYSTAL_PLORT),
-        (Identifiable.Id.HUNTER_SLIME, Identifiable.Id.HUNTER_PLORT),
-        (Identifiable.Id.HONEY_SLIME, Identifiable.Id.HONEY_PLORT),
-        (Identifiable.Id.QUANTUM_SLIME, Identifiable.Id.QUANTUM_PLORT),
-        (Identifiable.Id.DERVISH_SLIME, Identifiable.Id.DERVISH_PLORT),
-        (Identifiable.Id.TANGLE_SLIME, Identifiable.Id.TANGLE_PLORT),
-        (Identifiable.Id.MOSAIC_SLIME, Identifiable.Id.MOSAIC_PLORT),
-        (Identifiable.Id.PUDDLE_SLIME, Identifiable.Id.PUDDLE_PLORT),
-        (Identifiable.Id.FIRE_SLIME, Identifiable.Id.FIRE_PLORT),
-    ];
-
-    public static EconomyDirector.ValueMap GetValueMap(Identifiable.Id id)
+    private static EconomyDirector.ValueMap GetValueMap(Identifiable.Id id)
     {
         // Everything that can be sold
 
@@ -42,17 +23,36 @@ public static class ExtraSlimes
         return null;
     }
 
-    public static void RegisterSlime(Identifiable.Id slimeId, Identifiable.Id plortId, float multiplier = 4f, float basePrice = 0f, float slimeSaturation = 0f)
+    private static MarketUI.PlortEntry GetPlortEntry(Identifiable.Id id)
+    {
+        foreach (var entry in Main.PlortsToPatch)
+        {
+            if (entry.id == id)
+                return entry;
+        }
+
+        return null;
+    }
+
+    public static void RegisterSlime
+    (
+        Identifiable.Id slimeId,
+        Identifiable.Id plortId,
+        float multiplier = 4f,
+        float basePrice = 0f,
+        float slimeSaturation = 0f,
+        ProgressDirector.ProgressType[] progress = null
+    )
     {
         var valueMap = GetValueMap(plortId);
         var (value, saturation) = slimeId switch
         {
             _ when valueMap != null => (valueMap.value, valueMap.fullSaturation),
-            Identifiable.Id.QUICKSILVER_SLIME => (basePrice, slimeSaturation),
-            Identifiable.Id.LUCKY_SLIME => (basePrice, slimeSaturation),
-            _ => throw new Exception($"Could not find sellable plort data for {plortId} for slime {slimeId}")
+            _ when basePrice != 0f && slimeSaturation != 0f => (basePrice, slimeSaturation),
+            _ => throw new Exception($"Could not find/set up sellable plort data for {plortId} for slime {slimeId}")
         };
-        PlortRegistry.RegisterPlort(slimeId, value * multiplier, saturation);
+        PlortRegistry.AddEconomyEntry(slimeId, value * multiplier, saturation); // Create a market entry
+        PlortRegistry.AddPlortEntry(slimeId, progress ?? GetPlortEntry(plortId)?.toUnlock ?? []); // Allow progress tracking
         DroneRegistry.RegisterBasicTarget(slimeId); // And make so that the drones can grab slimes
     }
 }
--- SlimesAndMarket/Main.cs
+++ SlimesAndMarket/Main.cs
@@ -4,44 +4,57 @@ using SRML.SR;
 
 namespace SlimesAndMarket;
 
-public class Main : ModEntryPoint
+public sealed class Main : ModEntryPoint
 {
     public static Main Instance { get; private set; }
+
     public static List<EconomyDirector.ValueMap> ValuesToPatch;
+    public static List<MarketUI.PlortEntry> PlortsToPatch;
+
+    private static readonly List<(Identifiable.Id, Identifiable.Id, ProgressDirector.ProgressType[])> VANILLA_SLIMES =
+    [
+        (Identifiable.Id.PINK_SLIME, Identifiable.Id.PINK_PLORT, []),
+        (Identifiable.Id.TABBY_SLIME, Identifiable.Id.TABBY_PLORT, []),
+        (Identifiable.Id.ROCK_SLIME, Identifiable.Id.ROCK_PLORT, []),
+        (Identifiable.Id.PHOSPHOR_SLIME, Identifiable.Id.PHOSPHOR_PLORT, []),
+        (Identifiable.Id.RAD_SLIME, Identifiable.Id.RAD_PLORT, [ProgressDirector.ProgressType.UNLOCK_QUARRY]),
+        (Identifiable.Id.BOOM_SLIME, Identifiable.Id.BOOM_PLORT, [ProgressDirector.ProgressType.UNLOCK_QUARRY, ProgressDirector.ProgressType.UNLOCK_MOSS]),
+        (Identifiable.Id.CRYSTAL_SLIME, Identifiable.Id.CRYSTAL_PLORT, [ProgressDirector.ProgressType.UNLOCK_QUARRY]),
+        (Identifiable.Id.HUNTER_SLIME, Identifiable.Id.HUNTER_PLORT, [ProgressDirector.ProgressType.UNLOCK_MOSS]),
+        (Identifiable.Id.HONEY_SLIME, Identifiable.Id.HONEY_PLORT, [ProgressDirector.ProgressType.UNLOCK_MOSS]),
+        (Identifiable.Id.QUANTUM_SLIME, Identifiable.Id.QUANTUM_PLORT, [ProgressDirector.ProgressType.UNLOCK_RUINS]),
+        (Identifiable.Id.DERVISH_SLIME, Identifiable.Id.DERVISH_PLORT, [ProgressDirector.ProgressType.UNLOCK_DESERT]),
+        (Identifiable.Id.TANGLE_SLIME, Identifiable.Id.TANGLE_PLORT, [ProgressDirector.ProgressType.UNLOCK_DESERT]),
+        (Identifiable.Id.MOSAIC_SLIME, Identifiable.Id.MOSAIC_PLORT, [ProgressDirector.ProgressType.UNLOCK_DESERT]),
+        (Identifiable.Id.PUDDLE_SLIME, Identifiable.Id.PUDDLE_PLORT, [ProgressDirector.ProgressType.UNLOCK_QUARRY, ProgressDirector.ProgressType.UNLOCK_MOSS]),
+        (Identifiable.Id.FIRE_SLIME, Identifiable.Id.FIRE_PLORT, [ProgressDirector.ProgressType.UNLOCK_DESERT]),
+    ];
 
     public override void PreLoad()
     {
         Instance = this;
         HarmonyInstance.PatchAll();
+
+        ValuesToPatch = AccessTools.Field(typeof(PlortRegistry), "valueMapsToPatch").GetValue(null) as List<EconomyDirector.ValueMap>;
+        PlortsToPatch = AccessTools.Field(typeof(PlortRegistry), "plortsToPatch").GetValue(null) as List<MarketUI.PlortEntry>;
     }
 
     public override void Load()
     {
-        ConsoleInstance.Log("Loading SlimesAndMarket");
+        VANILLA_SLIMES.ForEach(x => ExtraSlimes.RegisterSlime(x.Item1, x.Item2, progress: x.Item3));
 
-        try // Try the code there, if some error arises then it calls the "catch" function
+        // Only loading the special slime sales if relevant mods are enabled, because there would be no other way the player would be able to sell them otherwise
+
+        if (SRModLoader.IsModPresent("komikspl_quicksilver_rancher"))
+            ExtraSlimes.RegisterSlime(Identifiable.Id.QUICKSILVER_SLIME, Identifiable.Id.QUICKSILVER_PLORT, 5f, 200f, 100f, [ProgressDirector.ProgressType.UNLOCK_MOCHI_MISSIONS]);
+
+        if (SRModLoader.IsModPresent("puresaberslime"))
+            ExtraSlimes.RegisterSlime(Identifiable.Id.SABER_SLIME, Identifiable.Id.SABER_PLORT, 50f, progress: [ProgressDirector.ProgressType.UNLOCK_OGDEN_MISSIONS]);
+
+        if (SRModLoader.IsModPresent("more_vaccing"))
         {
-            ValuesToPatch = AccessTools.Field(typeof(PlortRegistry), "valueMapsToPatch").GetValue(null) as List<EconomyDirector.ValueMap>;
-
-            ExtraSlimes.VANILLA_SLIMES.ForEach(x => ExtraSlimes.RegisterSlime(x.Item1, x.Item2));
-
-            // Only loading the special slime sales if relevant mods are enabled, because there would be no other way the player would be able to sell them otherwise
-
-            if (SRModLoader.IsModPresent("komikspl_quicksilver_rancher"))
-                ExtraSlimes.RegisterSlime(Identifiable.Id.QUICKSILVER_SLIME, Identifiable.Id.QUICKSILVER_PLORT, 5f, 200f, 100f);
-
-            if (SRModLoader.IsModPresent("puresaberslime"))
-                ExtraSlimes.RegisterSlime(Identifiable.Id.SABER_SLIME, Identifiable.Id.SABER_PLORT, 50f);
-
-            if (SRModLoader.IsModPresent("more_vaccing"))
-            {
-                ExtraSlimes.RegisterSlime(Identifiable.Id.GOLD_SLIME, Identifiable.Id.GOLD_PLORT, 10f);
-                ExtraSlimes.RegisterSlime(Identifiable.Id.LUCKY_SLIME, 0, 25f, 250f, 125f);
-            }
-        }
-        catch (Exception e) // But if an error was found
-        {
-            ConsoleInstance.LogError("Slimes and Market mod error: " + e); // Then log the error into the SRML.log file
+            ExtraSlimes.RegisterSlime(Identifiable.Id.GOLD_SLIME, Identifiable.Id.GOLD_PLORT, 10f);
+            ExtraSlimes.RegisterSlime(Identifiable.Id.LUCKY_SLIME, 0, 25f, 250f, 125f);
         }
     }
 }
